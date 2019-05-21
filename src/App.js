707b2f410a5b7b3e445778dd2981db03a93d7dd1
popemray00 <popemray1986@gmail.com>
2019-05-20 22:13:46 -0700

    about me complete

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Route exact path="/" component={Home}/>;
+        <Route exact path="/" component={Home}/>
       </Router>
     </div>
   );
--- src/Home.js
+++ src/Home.js
@@ -5,21 +5,19 @@ class Home extends React.Component {
   render() {
     return (
       <div>
-        <div className="Header">
+        <div className="jumbotron jumbotron-fluid">
+        <div className="container">
           <h1> Michael Pope </h1>
         </div>
+        </div>
 
-        <div className="About">
-          <div className="AboutImg">
-            <img className="HomePic"
-            src = "images/homepic.jpg"
+        <div className="container-fluid">
+            <img className="SelfPic"
+            src = "images/self1.jpg"
             alt = "Michael"
             />
-          </div>
-          <div className="AboutMe">
-            <h2> About Me </h2>
-            <p></p>
-          </div>
+            <h1> About Me </h1>
+            <p>I'm a full stack web developer with a passion for both Front and Backend development. With experience in Ruby, Ruby on Rails, JavaScript, React and knowledge with many more technologies. I'm coming from a background in sales and customer service. I discovered web development through a podcast called CodeNewbie which motivated me to pursue my education at The Flatiron School in this field. I bring strong skills in problem solving, ability to learn quickly, customer service/relations and sales. My goal is to provide tremendous value through reliable code and dedication to others. I'm always on the search for opportunities to learn and grow as a developer. </p>
         </div>
       </div>
     );
